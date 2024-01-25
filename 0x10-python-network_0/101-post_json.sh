4a4486a0b86f90e0a6a5ef22c011b8c1f55ee9ae
agichimu <alexandergichimu98@gmail.com>
2024-01-25 22:53:31 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# This script sends a JSON POST request to a URL with the contents of a file and displays the body of the response
+if [ -f "$2" ]; then
+    curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+else
+    echo "Not a valid JSON"
+fi
