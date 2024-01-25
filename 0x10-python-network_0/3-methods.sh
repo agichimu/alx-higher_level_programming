f6852e5f480f8c8139bad890ec1ca300ea09b1cd
agichimu <alexandergichimu98@gmail.com>
2024-01-25 22:50:43 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# This script takes a URL and displays all HTTP methods the server will accept
+curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{gsub(",", ""); for(i=2;i<=NF;++i)print $i}'
